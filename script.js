9abbb40d452957d0d53e3b996db2df3c72d981e5
MiroslavKop <kopytovmiroslav@gmail.com>
2022-09-04 20:27:51 +0300

    func with using reduce, spread

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 "use strict";
 
+// ------forEach, concat--------------------/
+
+// function flatten(arr) {
+//   let newArr = [];
+//   arr.forEach((element) => {
+//     if (Array.isArray(element)) {
+//       newArr = newArr.concat(flatten(element));
+//     } else {
+//       newArr.push(element);
+//     }
+//   });
+//   return newArr;
+// }
+
+// ------reduce, spread--------------------/
+
 function flatten(arr) {
-  let newArr = [];
-  arr.forEach((element) => {
+  let flatArray = [...arr];
+  flatArray = arr.reduce((acc, element) => {
     if (Array.isArray(element)) {
-      newArr = newArr.concat(flatten(element));
+      return [...acc, ...flatten(element)];
     } else {
-      newArr.push(element);
+      return [...acc, element];
     }
-  });
-  return newArr;
+  }, []);
+  return flatArray;
 }
 
 console.log(flatten([1, 2, {}, [3, [4], 5], [6, "seven"]]));
